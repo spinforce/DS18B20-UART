38d07778b02cead78943ed9b04ef2510f39a2dfc
spinforce <41535844+spinforce@users.noreply.github.com>
2018-07-22 17:35:45 +0200

    Add files via upload

---
+++ DS18B20UART/DS18B20.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.IO.Ports;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace DS18B20UART_OW
+{
+
+
+
+    class DS18B20
+    {
+
+        enum Bit { HIGH, LOW};
+        public enum Command {Search=0xF0,ReadRom=0x33,MatchRom=0x55,ConvertT=0x44, ReadScratchpad=0xbe, SkipRom=0xcc }
+
+        SerialPort _sPort;
+
+        public byte[] Buffer;
+        private byte Bit_last_Conflict_pos;
+        private const double Temp12 = 0.0625;
+        private const double Temp11 = 0.125;
+        private const double Temp10 = 0.25;
+        private const double Temp9 = 0.5;
+        public byte Devices;  
+       
+        bool LastDevice;
+
+ 
+
+        public DS18B20(string Port)
+        {
+            _sPort = new SerialPort();
+
+            //Command 1byte + Scratch 9byte oder Rom 8 byte 0-7 Command 8- Data
+            Buffer = new byte[8 * 10];
+
+            _sPort.PortName = Port;
+            _sPort.DataBits = 8;
+            _sPort.StopBits = StopBits.One;
+            _sPort.Parity = Parity.None;
+            _sPort.Open();
+
+            Bit_last_Conflict_pos = 0;
+            Devices = 0;
+            LastDevice = false;
+
+        }
+
+        ~DS18B20()
+        {
+            _sPort.Close();
+        }
+
+        public bool Reset()
+        {
+
+            _sPort.BaudRate = 9600;
+            Buffer[0] = 0xF0;
+            WriteCommandToPort(1); 
+
+            if (Buffer[0] != 0xF0)
+            {
+               if(Devices < 1 ) Devices = 1;
+                _sPort.BaudRate = 115200;
+                return true;
+            }
+            return false;
+        }
+
+       public bool Search(ref byte[] Address)
+        {
+            if (LastDevice || Devices < 1 ) return true;
+
+            ByteToBuffer(Command.Search, 0);
+            //Send Command
+            WriteCommandToPort(8);
+
+            System.Threading.Thread.Sleep(5);
+
+            //Clear Data Buffer(+8) from last Conflict
+            for (int ix = Bit_last_Conflict_pos + 1 + 8; ix < 8 * 8; ix++) Buffer[ix] = 0x0;
+
+            for (byte BitsPos = 0; BitsPos < 8 * 8; BitsPos++)
+            {
+                ByteToBuffer(3, 0); //0b00000011
+
+                //Get Payload
+                WriteCommandToPort(2);
+
+
+                switch (BufferToBytes(0,1)[0])
+                {
+                    case 0:
+                        //Konflikt 2 mögliche Pfade wähle erst 0 dann beim nächtsetn mal 1
+                         if (Buffer[BitsPos + 8] == 0x00) Bit_last_Conflict_pos = BitsPos;
+                        break;
+
+                    case 1:
+                        //Antwort Write 1
+                        SetBufferDataBit(BitsPos, Bit.HIGH);
+                        break;
+
+
+                    case 2:
+                        //Antwort Write 0
+                        SetBufferDataBit(BitsPos, Bit.LOW);
+                        break;
+
+                    case 3:
+                        //Fehler
+                        break;
+                }
+                //Im Datenbuffer ist das nächste Kommando enthalen
+                Buffer[0] = Buffer[BitsPos + 8];
+                WriteCommandToPort(1);
+
+            }
+
+            //aktuelles Ergebniss speichern
+            Address = BufferToBytes(8,8);
+
+            //Pfad,Buffer für nächsten Durchlauf vorbereiten
+            if (Buffer[Bit_last_Conflict_pos + 8] != 0xFF && Bit_last_Conflict_pos!=0)
+            {
+                SetBufferDataBit(Bit_last_Conflict_pos, Bit.HIGH);
+                Devices++;
+                LastDevice = false;
+            }
+            else LastDevice = true;
+            return false;
+
+        }
+
+
+        private void SetBufferDataBit(byte pos, Bit b)
+        {
+            if (b == Bit.HIGH) Buffer[pos+8] = 0xFF;
+            else Buffer[pos+8] = 0x00;
+        }
+
+
+        public byte[] Transfer(Command command)
+        {
+            return Transfer(command, 0);
+        }
+
+        public byte[] Transfer(Command command, byte count)
+        {
+          
+           if(command == Command.ReadScratchpad || command == Command.ReadRom)
+                    for (int ix = 0; ix < 8 * (count+1); ix++) Buffer[ix] = 0xFF;
+
+            ByteToBuffer(command, 0);
+            //Send Command
+            WriteCommandToPort(8);
+
+            System.Threading.Thread.Sleep(5);
+
+            if (count > 0) WriteDataToPort((byte)(count * 8));
+ 
+            //return BufferToBytes(0,(byte)(count + 1));
+            return BufferToBytes(8, count);
+
+        }
+
+        /// <summary>
+        /// Schreibe # Bits from Databuffer zum Port
+        /// </summary>
+        /// <param name="count">Nr. Bits to Write</param>
+        private void WriteDataToPort(byte count)
+        {
+
+            _sPort.Write(Buffer, 8, count);
+            while (_sPort.BytesToRead != count) System.Threading.Thread.Sleep(1);
+            _sPort.Read(Buffer, 8, count);
+
+        }
+
+
+        /// <summary>
+        /// Schreibe # Bits vom Commandbuffer zum Port
+        /// </summary>
+        /// <param name="count">Nr. Bits to Write</param>
+        private void WriteCommandToPort(byte count)
+        {
+            _sPort.Write(Buffer, 0, count);
+            while (_sPort.BytesToRead != count) System.Threading.Thread.Sleep(1);
+            _sPort.Read(Buffer, 0, count);
+        }
+        
+ 
+        public void ByteToBuffer(byte b, byte pos)
+        {
+            byte rol = 0x1;
+
+            for (int ix = pos * 8; ix < pos * 8 + 8; ix++)
+            {
+                if ((b & rol) == 0) Buffer[ix] = 0;
+                else Buffer[ix] = 0xff;
+                rol <<= 1;
+            }
+        }
+        private void ByteToBuffer(Command c, byte pos)
+        {
+            ByteToBuffer((byte)c, pos);
+        }
+
+        public byte[] BufferToBytes(byte offset, byte count)
+        {
+
+            byte[] bytes;
+            bytes = new byte[count];
+            int rBufferPointer = offset;
+
+
+            for (int iy = 0; iy < count; iy++)
+            {
+                byte rol = 1;
+                byte b = 0;
+
+                for (int ix = 0; ix < 8; ix++)
+                {
+                    if (Buffer[rBufferPointer] == 0xFF) b |= rol;                    
+                    else Buffer[rBufferPointer] = 0;
+
+                    rBufferPointer++;
+                    rol <<= 1;
+                }
+                bytes[iy] = b;
+            }
+            return bytes;
+        }
+
+        public double GetTemp()
+        {
+
+            double t_result;
+            byte[] bytes = BufferToBytes(8, 2);
+            int t = (bytes[1] << 8) | bytes[0];
+            if ((t & 0xf800) != 0) t_result = -(double)~(t - 1);
+            else t_result = t;
+            t_result *= Temp12;
+
+            return t_result;
+
+        }
+    }
+}
---
+++ DS18B20UART/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using DS18B20UART_OW;
+
+
+namespace DS18B20UART
+{
+    class Program
+    {
+        //static byte[] mySensor = { 0x28, 0x95, 0xa8, 0x27, 0x00, 0x00, 0x80, 0x33 };
+        //static byte[] mySensor = { 0x28, 0x3a, 0xf9, 0x27, 0x00, 0x00, 0x80, 0x13 };
+       // static byte[] mySensor = { 0x28, 0x77, 0x99, 0x27, 0x0, 0x0, 0x80, 0x3d }; 
+
+
+        static byte[] bytes;
+        static byte[] Address = new byte[8];
+
+        static ArrayList Sensors = new ArrayList(8);
+
+
+
+        static DS18B20 sensor = new DS18B20("COM16");
+
+        
+
+
+        static void Main(string[] args)
+        {
+
+
+            //********************* Ist wer da ? ********************************
+            Console.Clear();
+
+            if (!sensor.Reset())
+            {
+                Console.WriteLine("Nix Sensor\r\n");
+                return;
+            }
+
+            Console.WriteLine("Sensor da :-)\r\n");
+
+
+
+
+            while (!sensor.Search(ref Address))
+            {
+
+                Console.Write("Sensor ");
+
+                PrintBytes(Address);
+
+                Sensors.Add(Address);
+
+                sensor.Reset();
+
+            }
+
+            Console.WriteLine("{0} Sensor(en) gefunden", sensor.Devices);
+            System.Threading.Thread.Sleep(2000);
+
+
+            //********************* Nur ein Sensor am Bus. Wer da ist da ? ********************************
+
+            /*
+                        bytes = ow.Transfer(0x33, 8, OnWire.TransferDirection.Read);
+                        
+
+                        Console.Write("Memory:");
+                        for (byte ix = 1; ix < bytes.Length - 1; ix++) Console.Write("{0:x2}:", bytes[ix]);
+                        Console.WriteLine("{0:x2}\r\n", bytes[bytes.Length - 1]);
+
+    */
+
+
+
+            while (true)
+            {
+                Console.Clear();
+
+                //Skip Rom
+                StartConvertion(true);
+
+                for (byte iy = 0; iy < Sensors.Count; iy++)
+                {
+                    for (byte ix = 0; ix < 8; ix++) sensor.ByteToBuffer(((byte[])Sensors[iy])[ix], (byte)(ix + 1));
+
+                    //Ohne SkipRom
+                   // StartConvertion(false);
+                    ReadScratchPad();
+
+                }
+                System.Threading.Thread.Sleep(5000);
+            }
+
+        }
+
+        static void PrintBytes(byte[] b)
+        {
+            for (byte ix = 0; ix < b.Length - 1; ix++) Console.Write("{0:x2}:", b[ix]);
+            Console.WriteLine("{0:x2}", b[b.Length - 1]);
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="SkipRom">true SkipROM oder nicht</param>
+        static void StartConvertion(bool SkipRom)
+        {
+            //********************* Start Convertion ********************************
+
+            Console.WriteLine("Reset Bus");
+            if (!sensor.Reset())
+            {
+                Console.WriteLine("Nix Sensor\r\n");
+                return;
+            }
+
+
+            //Select Sensor
+            Console.Write("Select Sensor ");
+
+            Address = sensor.BufferToBytes(8, 8);
+            PrintBytes(Address);
+
+
+            //Transfer Command und Sensor Adresse 8 Bytes
+            if (SkipRom) sensor.Transfer(DS18B20.Command.SkipRom);
+            else sensor.Transfer(DS18B20.Command.MatchRom, 8);
+
+
+            //Start Temp Convertion nur Command
+            Console.WriteLine("Start Convertion");
+            sensor.Transfer(DS18B20.Command.ConvertT);
+
+            //Datenblatt max Convertion Time
+            System.Threading.Thread.Sleep(800);
+
+        }
+
+        static void ReadScratchPad()
+        {
+
+            //********************* Read Scratchpad ********************************
+            Console.WriteLine("Reset Bus");
+            if (!sensor.Reset())
+            {
+                Console.WriteLine("Nix Sensor\r\n");
+                return;
+            }
+
+            //Select Sensor
+            Console.WriteLine("Select Sensor wie vorher");
+            sensor.Transfer(DS18B20.Command.MatchRom, 8);
+
+            Console.WriteLine("Read Scratchpad");
+            bytes = sensor.Transfer(DS18B20.Command.ReadScratchpad, 9);
+
+
+            Console.Write("Scratchpad ");
+            PrintBytes(bytes);
+
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Temp: {0}°C\r\n", sensor.GetTemp());
+            Console.ResetColor();
+        }
+    }
+
+}
